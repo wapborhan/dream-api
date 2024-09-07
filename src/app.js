26afa8ad9e22af7ecfa178690240267669ca9789
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2024-09-07 20:22:21 +0600

    Homepage set Client Side

--- src/app.js
+++ src/app.js
@@ -12,6 +12,10 @@ app.set("views", path.join(__dirname, "public"));
 // app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "../client/dist/")));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
 // midlewares
 applyMiddlewares(app);
 
